5b49782e0d3d980a2e74504aaaec014719aa1ccb
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-10-12 15:30:11 -0300

    pull bd

--- cadastro.php
+++ cadastro.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/globals.css">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="styles/cadastro.css">
     <title>Cadastrar Usuário</title>
 </head>
 <body>
     <main class="mainLogin">
-    <form action="" class="formLogin">
+        <form action="" class="formLogin">
             <fieldset class="containerLogin">
                 <legend>Não é cadastrado? Faça agora!</legend>
                 <div>
