90a74bd2d0604c0c7d93bb830325b99f5124ed15
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-10-12 18:22:02 -0300

    correcao de erros no html

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="styles/home.css">
+    <link rel="stylesheet" href="styles/globals.css">
     <link rel="stylesheet" href="styles/home.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
