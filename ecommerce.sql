f5c51999baa73430856f63580ade721b09d5a31f
Paulo Almeida <paulo333henrique00@gmail.com>
2022-12-11 16:48:07 -0300

    Add files via upload

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,40 +1,33 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 04-Out-2022 às 19:39
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.3.3
+-- Host: localhost
+-- Tempo de geração: 11/12/2022 às 20:46
+-- Versão do servidor: 10.4.24-MariaDB
+-- Versão do PHP: 8.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `ecommerce`
+-- Banco de dados: `ecommerce`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cadastro`
+-- Estrutura para tabela `cadastro`
 --
 
 CREATE TABLE `cadastro` (
   `id_cadastro` int(11) NOT NULL,
-  `nome` varchar(30) NOT NULL,
+  `nome` varchar(50) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `senha` int(11) NOT NULL,
-  `telefone` int(11) NOT NULL,
-  `CPF` int(11) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `CPF` varchar(14) NOT NULL,
   `cidade` varchar(45) NOT NULL,
   `CEP` int(11) NOT NULL,
   `estado` varchar(20) NOT NULL,
@@ -43,53 +36,69 @@ CREATE TABLE `cadastro` (
   `numero` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produtos`
+-- Estrutura para tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
   `id_produtos` int(11) NOT NULL,
   `nome` varchar(30) NOT NULL,
   `descricao` varchar(80) NOT NULL,
+  `imagem` varchar(100) NOT NULL,
   `preco` double NOT NULL,
   `promocao` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produtos`, `nome`, `descricao`, `imagem`, `preco`, `promocao`) VALUES
+(1, 'Placa de vídeo', 'placa de vídeo', 'placadevideo.jpg', 500, 0),
+(3, 'cooler', 'cooler foda', 'cooler.jpg', 12, 1),
+(5, 'cabo ethernet', 'cabo ethernet clássico', 'ethernet.jpg', 20, 0),
+(7, 'gabinete', 'gabinete gamer', 'gabinete.jpg', 800, 0),
+(9, 'monitor', 'monitor gamer', 'monitor.png', 2000, 1),
+(11, 'mouse', 'mouse gamer', 'mouse.jpg', 200, 1),
+(13, 'Playstation 5', 'aquele que parece ar-condicionado', 'ps5.png', 4000, 0),
+(15, 'ryzen', 'só perde pro celeron', 'ryzen.jpg', 900, 1),
+(17, 'xbox', 'microsoft fazendo fama', 'xbox.jpg', 4000, 0);
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `cadastro`
+-- Índices de tabela `cadastro`
 --
 ALTER TABLE `cadastro`
-  ADD PRIMARY KEY (`id_cadastro`);
+  ADD PRIMARY KEY (`id_cadastro`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `CPF` (`CPF`);
 
 --
--- Indexes for table `produtos`
+-- Índices de tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id_produtos`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `cadastro`
+-- AUTO_INCREMENT de tabela `cadastro`
 --
 ALTER TABLE `cadastro`
-  MODIFY `id_cadastro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cadastro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
--- AUTO_INCREMENT for table `produtos`
+-- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produtos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produtos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
